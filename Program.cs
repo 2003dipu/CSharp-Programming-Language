57b1e86a4c4203d82dddc4d4009e9c7c59a86603
Dipu Singha <137743843+2003dipu@users.noreply.github.com>
2023-09-09 22:40:32 +0600

    Random Module mastery
    Master the Random module and become exceptional C# programmer

--- Program.cs
+++ Program.cs
@@ -1,128 +1,196 @@
-﻿// Shift + Alt + F = to format document
+﻿/*
+Random Module in C# 
+Console.WriteLine("-----------------Example 1------------------------");
+Console.WriteLine(
+    "This example of the Random.Next() methods\n" +
+    "generates the following output.\n");
+Console.WriteLine(
+    "Create Random objects all with the same seed and " +
+    "generate\nsequences of numbers with different " +
+    "bounds. Note the effect\nthat the various " +
+    "combinations of bounds have on the sequences.");
 
-// initialize variables - graded assignments
-int examAssignments = 5;
+NoBoundsRandoms(234);
 
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
-int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
-int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
-int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
-int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
+UpperBoundRandoms(234, Int32.MaxValue);
+UpperBoundRandoms(234, 2000000000);
+UpperBoundRandoms(234, 200000000);
 
-// Student names
-string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+BothBoundsRandoms(234, 0, Int32.MaxValue);
+BothBoundsRandoms(234, Int32.MinValue, Int32.MaxValue);
+BothBoundsRandoms(234, -2000000000, 2000000000);
+BothBoundsRandoms(234, -200000000, 200000000);
+BothBoundsRandoms(234, -2000, 2000);
 
-int[] studentScores = new int[10];
-
-string currentStudentLetterGrade = "";
-
-// Write the Report Header to the console
-Console.WriteLine("Student\t\tGrade\n");
-
-foreach (string name in studentNames)
+// Generate random numbers with no bounds specified.
+void NoBoundsRandoms(int seed)
 {
-    string currentStudent = name;
+    Console.WriteLine(
+        "\nRandom object, seed = {0}, no bounds:", seed);
+    Random randObj = new Random(seed);
 
-    if (currentStudent == "Sophia")
-        studentScores = sophiaScores;
-
-    else if (currentStudent == "Andrew")
-        studentScores = andrewScores;
-
-    else if (currentStudent == "Emma")
-        studentScores = emmaScores;
-
-    else if (currentStudent == "Logan")
-        studentScores = loganScores;
-
-    else if (currentStudent == "Becky")
-        studentScores = beckyScores;
-
-    else if (currentStudent == "Chris")
-        studentScores = chrisScores;
-
-    else if (currentStudent == "Eric")
-        studentScores = ericScores;
-
-    else if (currentStudent == "Gregor")
-        studentScores = gregorScores;
-    else
-        continue;
-
-    // initialize/reset the sum of scored assignments
-    int sumAssignmentScores = 0;
-
-    // initialize/reset the calculated average of exam + extra credit scores
-    decimal currentStudentGrade = 0;
-
-    // initialize/reset a counter for the number of assignment 
-    int gradedAssignments = 0;
-
-    // loop through the scores array and complete calculations for currentStudent
-    foreach (int score in studentScores)
-    {
-        // increment the assignment counter
-        gradedAssignments += 1;
-
-        if (gradedAssignments <= examAssignments)
-            // add the exam score to the sum
-            sumAssignmentScores += score;
-
-        else
-            // add the extra credit points to the sum - bonus points equal to 10% of an exam score
-            sumAssignmentScores += score / 10;
-    }
-
-    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
-
-    if (currentStudentGrade >= 97)
-        currentStudentLetterGrade = "A+";
-
-    else if (currentStudentGrade >= 93)
-        currentStudentLetterGrade = "A";
-
-    else if (currentStudentGrade >= 90)
-        currentStudentLetterGrade = "A-";
-
-    else if (currentStudentGrade >= 87)
-        currentStudentLetterGrade = "B+";
-
-    else if (currentStudentGrade >= 83)
-        currentStudentLetterGrade = "B";
-
-    else if (currentStudentGrade >= 80)
-        currentStudentLetterGrade = "B-";
-
-    else if (currentStudentGrade >= 77)
-        currentStudentLetterGrade = "C+";
-
-    else if (currentStudentGrade >= 73)
-        currentStudentLetterGrade = "C";
-
-    else if (currentStudentGrade >= 70)
-        currentStudentLetterGrade = "C-";
-
-    else if (currentStudentGrade >= 67)
-        currentStudentLetterGrade = "D+";
-
-    else if (currentStudentGrade >= 63)
-        currentStudentLetterGrade = "D";
-
-    else if (currentStudentGrade >= 60)
-        currentStudentLetterGrade = "D-";
-
-    else
-        currentStudentLetterGrade = "F";
-
-    //Console.WriteLine("Student\t\tGrade\tLetter Grade\n");
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+    // Generate six random integers from 0 to int.MaxValue.
+    for (int j = 0; j < 6; j++)
+        Console.Write("{0,11} ", randObj.Next());
+    Console.WriteLine();
 }
 
-// required for running in VS Code (keeps the Output windows open to view results)
-Console.WriteLine("\n\rPress the Enter key to continue");
-Console.ReadLine();
-// The above code takes 127 lines to accomplish the task.
-// I will find out or create a shortcut or alternative code soon.
+// Generate random numbers with an upper bound specified.
+void UpperBoundRandoms(int seed, int upper)
+{
+    Console.WriteLine(
+        "\nRandom object, seed = {0}, upper bound = {1}:",
+        seed, upper);
+    Random randObj = new Random(seed);
+
+    // Generate six random integers from 0 to the upper bound.
+    for (int j = 0; j < 6; j++)
+        Console.Write("{0,11} ", randObj.Next(upper));
+    Console.WriteLine();
+}
+
+// Generate random numbers with both bounds specified.
+void BothBoundsRandoms(int seed, int lower, int upper)
+{
+    Console.WriteLine(
+        "\nRandom object, seed = {0}, lower = {1}, " +
+        "upper = {2}:", seed, lower, upper);
+    Random randObj = new Random(seed);
+
+    // Generate six random integers from the lower to
+    // upper bounds.
+    for (int j = 0; j < 6; j++)
+        Console.Write("{0,11} ",
+            randObj.Next(lower, upper));
+    Console.WriteLine();
+}
+
+Console.WriteLine("-----------------Example 2------------------------");
+
+Random rnd = new Random();
+string[] malePetNames = { "Rufus", "Bear", "Dakota", "Fido",
+                          "Vanya", "Samuel", "Koani", "Volodya",
+                          "Prince", "Yiska" };
+string[] femalePetNames = { "Maggie", "Penny", "Saya", "Princess",
+                            "Abby", "Laila", "Sadie", "Olivia",
+                            "Starlight", "Talla" };
+
+// Generate random indexes for pet names.
+int mIndex = rnd.Next(malePetNames.Length);
+int fIndex = rnd.Next(femalePetNames.Length);
+
+// Display the result.
+Console.WriteLine("Suggested pet name of the day: ");
+Console.WriteLine("   For a male:     {0}", malePetNames[mIndex]);
+Console.WriteLine("   For a female:   {0}", femalePetNames[fIndex]);
+
+// The example displays output similar to the following:
+//       Suggested pet name of the day:
+//          For a male:     Koani
+//          For a female:   Maggie
+
+Console.WriteLine("-----------------Example 3------------------------");
+// Generating Random integer for a certain range
+// Random rnd = new Random(); use this line or the nest line
+Random rnd = new(); // alternative to the previous line(both options are available)
+
+Console.WriteLine("\n20 random integers from -100 to 100:");
+for (int ctr = 1; ctr <= 20; ctr++)
+{
+   Console.Write("{0,6}", rnd.Next(-100, 101));
+   if (ctr % 5 == 0) Console.WriteLine();
+}
+
+Console.WriteLine("\n20 random integers from 1000 to 10000:");
+for (int ctr = 1; ctr <= 20; ctr++)
+{
+   Console.Write("{0,8}", rnd.Next(1000, 10001));
+   if (ctr % 5 == 0) Console.WriteLine();
+}
+
+Console.WriteLine("\n20 random integers from 1 to 10:");
+for (int ctr = 1; ctr <= 20; ctr++)
+{
+   Console.Write("{0,6}", rnd.Next(1, 11));
+   if (ctr % 5 == 0) Console.WriteLine();
+}
+
+Console.WriteLine("-----------------Example 4------------------------");
+
+Random rnd = new Random(); // this does not generate a number currently
+string[] malePetNames = { "Rufus", "Bear", "Dakota", "Fido",
+                          "Vanya", "Samuel", "Koani", "Volodya",
+                          "Prince", "Yiska" };
+string[] femalePetNames = { "Maggie", "Penny", "Saya", "Princess",
+                            "Abby", "Laila", "Sadie", "Olivia",
+                            "Starlight", "Talla" };
+
+// Generate random indexes for pet names.
+int mIndex = rnd.Next(0, malePetNames.Length);
+int fIndex = rnd.Next(0, femalePetNames.Length);
+
+// Display the result.
+Console.WriteLine("Suggested pet name of the day: ");
+Console.WriteLine("   For a male:     {0}", malePetNames[mIndex]);
+Console.WriteLine("   For a female:   {0}", femalePetNames[fIndex]);
+
+
+Console.WriteLine("-----------------Example 5------------------------");
+
+Random random = new Random();
+string[] orderIDs = new string[5];
+// Loop through each blank orderID
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    // Get a random value that equates to ASCII letters A through E
+    int prefixValue = random.Next(65, 70);
+    // Convert the random value into a char, then a string
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, pad with zeroes
+    string suffix = random.Next(1, 1000).ToString("000");
+    // Combine the prefix and suffix together, then assign to current OrderID
+    orderIDs[i] = prefix + suffix;
+}
+// Print out each orderID
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+
+
+
+//  This code reverses a message, counts the number of times 
+ //  a particular character appears, then prints the results
+//   to the console window.
+ 
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string newMessage = new(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
+
+Random dice = new();
+int roll = dice.Next(1, 7);
+Console.WriteLine($"The randomly generated number is: {roll} ");
+*/
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
